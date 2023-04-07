f8363f9e3c93233caae3d5ea69c4947a0744fe63
resparx <ramprash.rspx@gmail.com>
2023-04-07 17:17:41 +0530

    feat: wip hero layout

---
+++ src/components/navbar.tsx
@@ -0,0 +1,30 @@
+/** @jsx jsx */
+import { jsx } from "theme-ui"
+import React from 'react';
+
+
+const Navbar = () => {
+    return <ul sx={{
+        color: 'white',
+        display: 'flex'
+    }}>
+        <image src="" />
+        <li>
+            Who we are
+        </li>
+        <li>
+            Service
+        </li>
+        <li>
+            Testimonials
+        </li>
+        <li>
+            Career
+        </li>
+        <li>
+            Contact
+        </li>
+    </ul>
+}
+
+export default Navbar
