6c19fe5fc3086142c9feec98284c7481a5562b90
Seo-hyge <seo.hygeinfotech@gmail.com>
2023-05-15 23:36:07 +0530

    Analytics Connector

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,15 @@ const Home = () => {
   return (
     <Fragment>
       <Head>
+      <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-736HQ07FJM"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-736HQ07FJM');
+</script>
         <title>Hyge Infotech</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta
