e854120857facc99ea3a3d3ceea6f3622e9ca3da
Seo-hyge <seo.hygeinfotech@gmail.com>
2023-05-16 12:19:52 +0530

    Google tag sitemap and robots update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,26 @@
 import React, { Fragment, useEffect } from "react";
 import Head from "next/head";
+import Script from "next/script"
 import { Footer, NavbarComponent, Preface } from "@components";
 
 const Home = () => {
   return (
     <Fragment>
-      <Head>
-      {/* <!-- Google tag (gtag.js) --> */}
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-736HQ07FJM"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-736HQ07FJM" id="googleTagmanager"></Script>
+        <Script src="../constants/googlescript.js" id="googleScript" >
+          
+        </Script>
 
-  gtag('config', 'G-736HQ07FJM');
-</script>
+        {/* Google Tag Manager */}
+        <Script strategy="afterInteractive" dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-MZMS9BS');`}} id="gtm">
+          </Script>
+      <Head>
+        {/*  Google tag (gtag.js)  */}
         <title>Hyge Infotech</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta
@@ -28,6 +34,8 @@ const Home = () => {
         ></link>
       </Head>
       <main style={{ overflowX: "hidden" }}>
+      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MZMS9BS" id="googleTM"
+  height="0" width="0" style={{display:"none",visibility:"hidden"}}></iframe></noscript>
         <NavbarComponent />
         <Preface />
         <Footer />
