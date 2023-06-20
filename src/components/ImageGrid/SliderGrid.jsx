16a425eaa67910e8da75fec0e76473739dabbe67
resparx <ramprash.rspx@gmail.com>
2023-06-20 17:55:03 +0530

    fix: add new carousel lib

--- src/components/ImageGrid/SliderGrid.jsx
+++ src/components/ImageGrid/SliderGrid.jsx
@@ -1,54 +1,36 @@
-import * as React from 'react';
-import { makeStyles, useTheme } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import MobileStepper from '@mui/material/MobileStepper';
-import SwipeableViews from 'react-swipeable-views';
-import { autoPlay } from 'react-swipeable-views-utils';
-
-const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
+import * as React from "react"
+import Box from "@mui/material/Box"
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
 
 const images = [
   {
-    label: 'San Francisco – Oakland Bay Bridge, United States',
-    imgPath:
-      '/gridImage/sectionOne.svg',
+    label: "San Francisco – Oakland Bay Bridge, United States",
+    imgPath: "/gridImage/sectionOne.svg",
   },
   {
-    label: 'Bird',
-    imgPath:
-    '/gridImage/sectionTwo.svg',
+    label: "Bird",
+    imgPath: "/gridImage/sectionTwo.svg",
   },
 ];
-
-const  SliderGrid =()=> {
-  const theme = useTheme();
+const SliderGrid = () => {
   const [activeStep, setActiveStep] = React.useState(0);
   const maxSteps = images.length;
 
-  const handleStepChange = (step) => {
-    setActiveStep(step);
-  };
-
   return (
-    <Box >
-      <AutoPlaySwipeableViews
-        axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
-        index={activeStep}
-        onChangeIndex={handleStepChange}
-        enableMouseEvents
-      >
+    <Box>
+      <Carousel>
         {images.map((step, index) => (
           <div key={step.label}>
             {Math.abs(activeStep - index) <= 2 ? (
               <Box
                 component="img"
-                
                 sx={{
-                  height: {lg:680,sm:300},
-                  display: 'block',
-                 
-                  overflow: 'hidden',
-                  width: '100%',
+                  height: { lg: 680, sm: 300 },
+                  display: "block",
+
+                  overflow: "hidden",
+                  width: "100%",
                 }}
                 src={step.imgPath}
                 alt={step.label}
@@ -56,19 +38,9 @@ const  SliderGrid =()=> {
             ) : null}
           </div>
         ))}
-      </AutoPlaySwipeableViews>
-      <MobileStepper
-      variant="dots"
-        steps={maxSteps}
-        position="static"
-        activeStep={activeStep}
-        classes={{
-            
-        }}
-        sx={{ maxWidth: 400, flexGrow: 1 ,justifyContent: 'center',margin:'auto'}}
-      />
+      </Carousel>
     </Box>
   );
-}
+};
 
 export default SliderGrid;
